228d35ab8e88b6ad6377d4bb96ad60a157dbefbf
qq3173732005 <stellayueyizhang@gmail.com>
2024-11-11 13:32:29 -0500

    Example gene expression matrix

---
+++ R/data.R
@@ -0,0 +1,15 @@
+# TODO: should we specify the source?
+
+#' SERGIO DS4 gene expression matrix
+#'
+#' The DS4 gene expression matrix generated with SERGIO
+#'
+#' @format ## `SERGIO_DS4_gene_expression_matrix`
+#' A gzipped data frame with 100 rows and 900 columns:
+#' \describe{
+#'   \item{Rows}{Unique gene names}
+#'   \item{Columns}{Unique sample names}
+#'   \item{Values}{Expression levels of each gene in each sample}
+#' }
+#' @source <https://github.com/hlab1/scRNAseqWithCICT/tree/main/inputs/SERGIO_DS4/net0>
+"SERGIO_DS4_gene_expression_matrix"
