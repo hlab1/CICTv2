ab0eb29f9b26d9c0abe68acef7240be908f597f8
qq3173732005 <stellayueyizhang@gmail.com>
2024-11-27 15:56:23 -0500

    Removed CRE dependency doFuture

--- R/calculateRawEdges.R
+++ R/calculateRawEdges.R
@@ -9,10 +9,6 @@ calculateRawEdges <- function(n.workers=5, in_data_obj=NULL, raw_edges=NULL, gen
 	nParallelThreads = 12
     edgeTypes <- cict_raw_edge_col
 
-
-
-library(doFuture)
-
 library(doSNOW)
 
 library(dplyr)
