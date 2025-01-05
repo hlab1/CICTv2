36d38c840895811e12a6e73536039b665e171585
govissers <gov2004@nyu.edu>
2025-01-05 12:00:22 -0500

    lintr to CRE

--- R/calculateRawEdges.R
+++ R/calculateRawEdges.R
@@ -1,16 +1,16 @@
-# NEW CRE
 
 
-#Mutual information steady state Multiple measures parallel
+calculateRawEdges <- function(
+	n.workers=5,
+	in_data_obj=NULL,
+	raw_edges=NULL,
+	gene_expression_matrix=NULL,
+	cict_raw_edge_col = 'Spearman',
+	in_format = "separate") {
 
-  #Parallel partition for each measure, multiple measures
 
-calculateRawEdges <- function(n.workers=5, in_data_obj=NULL, raw_edges=NULL, gene_expression_matrix=NULL, cict_raw_edge_col = 'Spearman',in_format = "separate") {
-	nParallelThreads = 12
-    edgeTypes <- cict_raw_edge_col
-
-
-########## all functions from before start here
+nParallelThreads = 12
+edgeTypes <- cict_raw_edge_col
 
 
 ################################################################################@
